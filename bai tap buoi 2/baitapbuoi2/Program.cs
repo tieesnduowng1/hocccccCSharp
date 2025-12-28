1370b5db2fb34824bf5b5b92c945780ab89376dc
tieesnduowng1 <vutienduong1904@gmail.com>
2025-12-28 22:29:41 +0700

    gui bai tap len

---
+++ bai tap buoi 2/baitapbuoi2/Program.cs
@@ -0,0 +1,145 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace baitapbuoi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 .in ra các biến cơ bản 
+
+            //    byte b = 255;
+            //    Console.WriteLine("byte\n"+b);
+            //    char c = 'D';
+            //    Console.WriteLine("char\n" + c);
+            //    bool bo=true;
+            //    Console.WriteLine("bool\n" + bo);
+            //    sbyte sb = -128;
+            //    Console.WriteLine("sbyte\n" + sb);
+            //    short s = 32767;
+            //    Console.WriteLine("short\n" + s);
+            //    ushort us = 65535;
+            //    Console.WriteLine("ushort\n" + us);
+            //    int i = 10000000;
+            //    Console.WriteLine("int\n" + i);
+            //    uint ui = 2929292929;
+            //    Console.WriteLine("uint\n" + ui);
+            //    float f = 3.1222f;
+            //    Console.WriteLine("float\n" + f);
+            //    double d = 2828229.3333;
+            //    Console.WriteLine("double\n" + d);
+            //    decimal de = 7383838.3883m;
+            //    Console.WriteLine("decimal\n" + de);
+            //    long l = 474848484848;
+            //    Console.WriteLine("long\n" + l);
+            //    ulong ul = 3838383833993939;
+            //    Console.WriteLine("ulong\n" + ul);
+            //    Console.ReadLine();
+
+
+           
+            
+            
+            
+            // 2. hiện tượng overflow
+            //byte b = 254;
+            //b++;
+            //Console.WriteLine( b);
+            //b++;
+            //Console.WriteLine( b);
+            //b++;
+            //Console.WriteLine( b);
+
+            //sbyte sb = 127;
+            //sb++;
+            //Console.WriteLine(sb);
+            //sb++;
+            //Console.WriteLine(sb);
+            //sb++;
+            //Console.WriteLine(sb);
+
+
+
+            
+            
+            
+            
+            
+            
+            //3. tạo chương trình tính toán dùng decimal
+            //Console.OutputEncoding=Encoding.UTF8;
+            //Console.WriteLine( "mời nhập số đầu tiên :");
+            //decimal S1=Convert.ToDecimal(Console.ReadLine ());
+            //Console.WriteLine( "mời nhập số thứ hai :");
+            //decimal S2=Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("mời bạn chọn phép toán cần tính ( +\t-\t*\t/) :");
+            //decimal tong=S1 +S2 ;
+            //decimal hieu=S1 -S2 ;
+            //decimal tich = S1 * S2;
+
+            //string dau=Console.ReadLine();
+            //if (dau == "+")
+            //    Console.WriteLine("tổng =\t"+tong);
+            //else if (dau == "-")
+            //    Console.WriteLine("hiệu =\t"+hieu);
+            //else if (dau == "*")
+            //    Console.WriteLine("tích =\t"+tich);
+            //else if (dau == "/")
+            //{
+
+            //    if (S2 == 0)
+            //        Console.WriteLine("phép tính không hợp lệ. ");
+            //    else
+            //    {
+            //        decimal thuong = S1 / S2;
+            //        Console.WriteLine("thương =" + thuong);
+            //    }
+            //}
+            //else
+            //    Console.WriteLine( "phép tính không hợp lệ.");
+            //Console.ReadLine();
+
+            
+            
+            
+            
+            
+            //4.boxing và unboxing
+            //Console.OutputEncoding = Encoding.UTF8;
+            //string a = "Vũ";
+            //a += "Tiến";
+            //a = a + "Dương";
+            //Console.WriteLine( a);
+            //object obj = 20;
+            //Console.WriteLine( obj);
+            //object obj2 = "Dương";
+            //Console.WriteLine( obj2);
+
+
+            
+            
+            
+            
+            
+            //5.độ chính xác của float double decimal
+            //Console.OutputEncoding=Encoding.UTF8;
+            //Console.WriteLine( "mời nhập số thứ nhất");
+            //int x1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("mời nhập số thứ hai");
+            //int x2 = Convert.ToInt32(Console.ReadLine()); ;
+            //float thuong1 =(float ) x1 / x2;
+            //double thuong2 =(double) x1 / x2;
+            //decimal thuong3 =(decimal) x1 / x2;
+            //Console.WriteLine("float"+thuong1);
+            //Console.WriteLine("double"+thuong2);
+            //Console.WriteLine("decimal"+thuong3);
+            
+
+              
+
+
+        }
+    }
+}
