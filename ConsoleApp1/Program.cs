067d7a433a88506dbd0403d7358dbbaf69da7a3f
tieesnduowng1 <vutienduong1904@gmail.com>
2025-12-28 17:43:33 +0700

    GUI CODE LAN DAU LEN GITHUB

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Text.Json;
+namespace ConsoleApp1
+{
+   internal class Program
+   {
+       static void Main(string[] args)
+       {
+            //            //Console.WriteLine("Hello, World!");
+            //            //Quy tac dat ten bien
+            //            // 1 bien cuc bo = camelCase
+            //            // 2 bien co y nghia = dat ten ro rang
+            //            //
+            //            string fullName = "Duong Vu";
+            //            //Dat ten bien theo tung loai
+            //            //1 Bien so
+            //            int count;
+            //            int maxValue;
+            //            double score;
+            //            //Bien chuoi(string)
+            //            string emailAddress;
+            //            //Bien mang 
+            //            int[] scores;
+            //            List<string> scoreList;
+            //            //4 Bien hang so(constant) = khai bao kieu PascalCase
+            //            const int TotalScore = 15;
+            //            //Khoi tao bien gan lai gia tri cho bien
+            //            //int x = 5;
+            //            //x = 10;
+            //            //Console.WriteLine( "gia tri x"+x);
+            //            //khai bao bien luc chay
+            //            string addressV2=Console.ReadLine ();
+            //            Console.WriteLine("dia chi nha duong "+addressV2 );
+
+
+
+            //            //comment nhanh
+            //            //ctrl /
+            //            //1 vai cau lenh trong c#
+            //            //ctrl+D nhanh dong
+            //            //ctrl+x di chuyen dong
+            //            //giu alt+keo chuot
+            //            //alt+mui ten len hoac xuong
+            //            //doi ten ctrl+r
+
+
+
+            //            //kieu du lieu bien
+            //            //1 kieu giu lieu gia tri
+            //            // kieu hay dung: int long double float
+            //            //so nguyen
+            //            int gioiHan = -2000000000;
+            //            long gioiHan1 = -3000000000000000;
+            //            //so thuc
+            //            float gioiHan2 = 2.3333333f;
+            //            double gioiHan3 = 46788.444;
+            //            //so thap phan
+            //            decimal gioiHan4 = 8449499494949.9499494m;
+            //            //kieu ky tu
+            //            char b = 'a';
+            //            //kieu logic
+            //            bool isCheck = false;
+            //            bool isCheck2 = true;
+            //            //2 kieu giu lieu tham chieu
+            //string
+            //object
+            //object className;
+            //className = "pc11";
+            //className = 20;
+            //boxing
+            //int a = 10;
+            //object obj =a;
+            //unboxing
+            //object obj = 10;
+            //int b = (int)obj;
+            //tai sao phai dung boxing vaf unboxing
+            //vi c# co kieu gia tri va tham chieu maf co luc c# chi lm vc voi obj lm cau noi
+            //4 kieu giu lieu con tro
+
+
+            //            //3 kieu giu lieu con tro
+            //char a = 'g';
+            //int i = 89;
+            //short s = 56;
+            //long l = 4564;
+            //uint ui = 95;
+            //ushort us = 76;
+            //ulong ul = 3667777;
+            //double d = 8.99494949;
+            //float f = 3.47477f;
+            //decimal dec = 64738.74m;
+            //Console.WriteLine( "char"+a);
+            //Console.WriteLine( "int"+i);
+            //Console.WriteLine( "long"+l);
+            //Console.WriteLine( "float"+f);
+            //Console.WriteLine( "double"+d);
+            //Console.WriteLine( "dicemal"+dec);
+            //Console.WriteLine( "uint"+ui);
+            //Console.WriteLine( "ushort"+us);
+            //Console.WriteLine( "ulong"+ul);
+            //Console.WriteLine( "moi nhap ten:");
+            //string ten=Console.ReadLine();
+            //Console.WriteLine( "chao"+ ten);
+            //Console.ReadLine();
+       }
+    }
+}
